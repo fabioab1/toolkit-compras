170796a03e395c1e381f0e7a483f58cb5afe41d9
fabioab1 <fabiobonanatodeabreu990@gmail.com>
2026-02-08 08:22:06 -0300

    feat: cria o projeto

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const btnCalcMed = document.getElementById('btn-calc-med');
+
+    const preco1 = document.getElementById('preco1');
+    const preco2 = document.getElementById('preco2');
+    const preco3 = document.getElementById('preco3');
+    const preco4 = document.getElementById('preco4');
+    const preco5 = document.getElementById('preco5');
+    const preco6 = document.getElementById('preco6');
+    const preco7 = document.getElementById('preco7');
+    const preco8 = document.getElementById('preco8');
+    const preco9 = document.getElementById('preco9');
+    const preco10 = document.getElementById('preco10');
+
+    function calcularMedia() {
+        
+        
+
+    }
+
+    btnCalcMed.addEventListener('click', function() {
+
+
+
+    })
+
+})
